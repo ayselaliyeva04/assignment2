9e66345752333db8b4baa7648f1f66bea6ff0280
ayselaliyeva04 <98714063+ayselaliyeva04@users.noreply.github.com>
2023-12-06 20:59:09 +0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+import org.example.BookStore;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        BookStore BookStore=new BookStore();
+        //BookStore.connectToBookStore("assignment2", "madinarustamova", "Medine2233!");
+        Connection conn=BookStore.connectToBookStore("assignment2", "madinarustamova", "Medine2233!");
+
+        //BookStore.createBook(conn, "5", "Harry Potter and the Sorcerer''s Stone", "1", "1984", "50", "1");
+        //BookStore.readBooks(conn,"");
+        //BookStore.updateBookStock(conn, "");
+        //BookStore.deleteBook(conn, "");
+        //BookStore.Order(conn, "");
+        //System.out.println(BookStore.Check(conn, ""));
+        //BookStore.OrderBook(conn, );
+        //BookStore.OrderBook(conn, "");
+    }
+}
+
